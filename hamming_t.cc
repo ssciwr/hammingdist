431e0fd1ddfff83caa4012d2cf20072a5bc86792
Liam Keegan <liam@keegan.ch>
2020-10-29 09:48:42 +0100

    add tests of distance between two chars

--- hamming_t.cc
+++ hamming_t.cc
@@ -4,6 +4,40 @@
 #include <string>
 #include <vector>
 
+constexpr std::array<char, 4> valid_chars{'A', 'C', 'G', 'T'};
+
+static int dist(char c1, char c2) {
+  return from_stringlist({std::string{c1}, std::string{c2}})[{0, 1}];
+}
+
+TEST_CASE("distance between two equal valid characters is 0", "[distance]") {
+  for (auto c : valid_chars) {
+    CAPTURE(c);
+    REQUIRE(dist(c, c) == 0);
+  }
+}
+
+TEST_CASE("distance between any valid char & '-' is 0", "[distance]") {
+  for (auto c : valid_chars) {
+    CAPTURE(c);
+    REQUIRE(dist(c, '-') == 0);
+    REQUIRE(dist('-', c) == 0);
+  }
+}
+
+TEST_CASE("distance between two different valid characters is 1",
+          "[distance]") {
+  for (auto c1 : valid_chars) {
+    for (auto c2 : valid_chars) {
+      if (c1 != c2) {
+        CAPTURE(c1);
+        CAPTURE(c2);
+        REQUIRE(dist(c1, c2) == 1);
+      }
+    }
+  }
+}
+
 TEST_CASE("from_stringlist", "[hamming]") {
   std::vector<std::string> v{{"ACGT"}, {"AGGG"}};
   auto d = from_stringlist(v);
